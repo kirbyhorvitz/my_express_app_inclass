28043fe039a522ce7e57f6314c0fed3ed60c7f48
kirbyhorvitz <kth49@georgetown.edu>
2021-05-12 23:59:12 -0400

    update router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
+// this is the "routes/users.js" file...
+// ...
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var users = [
+    {"id": 1, "name": "Jane McConnel", "email": "jane@example.com"},
+    {"id": 2, "name": "Earl Jones", "email": "earl@yahoo.com"},
+    {"id": 3, "name": "Sammy Student", "email": "sammy@myschool.edu"},
+  ] // just some dummy data
+  res.send(users);
 });
-
-module.exports = router;
+// ...
