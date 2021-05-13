61cadffc0a091edc922b8755eeb76e9c3fc5fe8c
kirbyhorvitz <kth49@georgetown.edu>
2021-05-13 00:28:30 -0400

    Setup another user route

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,14 @@ router.get('/', function(req, res, next) {
   res.send(users);
 });
 
+router.get('/:id', function(req, res, next) {
+  var userId = req.params.id;
+  console.log("USER ID:", userId)
+
+  var user = {"id": userId, "name": "Example User", "email": "example@example.com"} // just some dummy data
+  res.send(user);
+});
+
 module.exports = router;
 
 // ...
